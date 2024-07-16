089ddc32ba17e4800dad801b7e1355364e386625
garmin <zhangjiaming@ufactory.cc>
2024-07-16 17:13:54 +0800

    version

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,16 @@
 import { invoke } from '@tauri-apps/api';
-import { ConfigProvider, theme } from 'antd';
+import { getVersion } from '@tauri-apps/api/app';
+import { ConfigProvider, Tag, theme } from 'antd';
 import { useEffect, useState } from 'react';
 import { useTranslation } from 'react-i18next';
 import RouterConfig from './router';
 
 function App() {
+  const [version, setVersion] = useState<string>('');
   useEffect(() => {
+    getVersion().then((res) => {
+      setVersion(res);
+    });
     setTimeout(() => {
       document.addEventListener('DOMContentLoaded', () => {
         // 这将等待窗口加载，但你可以在您想要的任何触发器上运行此函数
@@ -19,7 +24,6 @@ function App() {
   // console.log('algorithm', theme.defaultAlgorithm);
 
   const { i18n } = useTranslation();
-  console.log('d===>', i18n);
 
   return (
     <ConfigProvider
@@ -44,6 +48,7 @@ function App() {
         >
           切换-{i18n.language}
         </button>
+        <Tag color="#108ee9">v{version}</Tag>
         <RouterConfig />
       </div>
     </ConfigProvider>
